b50c2dd92b2a5159ec998b7f2c584dc3335a1d76
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-03-26 21:09:49 -0300

    Training view temp

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,5 +1,6 @@
 import { ButtonHTMLAttributes, PropsWithChildren, Ref } from "react";
-import { cn } from "../Lib/class_names";
+import { cn } from "../../Lib/class_names";
+import style from "./style.module.css";
 
 export interface ButtonProps extends PropsWithChildren, ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
@@ -15,7 +16,8 @@ export default function Button(props: ButtonProps) {
     <button
       {...props}
       className={cn(
-        "bg-grey-0 border-grey-24 text-grey-24 shadow-pixel enabled:hover:shadow-pixel-sm enabled:active:bg-grey-2 disabled:bg-grey-8 disabled:text-grey-8 inline-flex items-center gap-4 border px-4 font-serif select-none enabled:active:translate-x-px enabled:active:translate-y-px enabled:active:shadow-none",
+        "bg-grey-0 border-grey-24 text-grey-24 shadow-pixel enabled:hover:shadow-pixel-sm enabled:active:bg-grey-2 disabled:text-grey-8 inline-flex items-center gap-4 border px-4 font-serif select-none enabled:active:translate-x-px enabled:active:translate-y-px enabled:active:shadow-none disabled:border-none disabled:shadow-none",
+        style.button,
         props.className
       )}
       ref={props.buttonRef}>
--- src/Views/home_view/index.tsx
+++ src/Views/home_view/index.tsx
@@ -1,16 +1,32 @@
 import AppFooter from "../../Components/AppFooter";
 import Button from "../../Components/Button";
+import { useWindowing } from "../../Lib/compass_navigator";
+import TrainingPage from "../training_view";
 
 export default function HomePage() {
+  const windowing = useWindowing();
+
+  function openTrainingPage() {
+    windowing.createWindow({
+      title: "Training",
+      component: TrainingPage,
+      props: {},
+    });
+  }
+
   return (
     <main className="bg-grey-1 relative flex h-full w-full flex-col gap-4 overflow-y-scroll font-serif">
       <nav className="border-grey-8 bg-grey-1 sticky top-0 z-10 mt-8 flex items-center gap-2 border-b p-4">
         <h1 className="text-xl">Home Page</h1>
       </nav>
       <section className="flex flex-col items-stretch gap-2 px-4">
-        <Button className="p-16 text-2xl">Treinamento</Button>
-        <Button className="p-16 text-2xl">Classificação</Button>
-        <Button className="p-16 text-2xl">Sobre o Aplicativo</Button>
+        <Button className="p-24 text-2xl" onClick={openTrainingPage}>
+          Treinamento
+        </Button>
+        <Button disabled className="p-24 text-2xl">
+          Classificação
+        </Button>
+        <Button className="p-24 text-2xl">Sobre o Aplicativo</Button>
       </section>
       <AppFooter />
     </main>
