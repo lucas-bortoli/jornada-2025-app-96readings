9372c237dc20b4a9f063be7b3dd950c988bb1921
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-04-07 11:54:31 -0300

    fix: Provide MiniGBus and BackButton

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BackButtonProvider } from "./Lib/back_button";
 import { WindowManagerProvider, WindowsOutlet } from "./Lib/compass_navigator";
+import { MiniGBusProvider } from "./Lib/gbus_mini";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <WindowManagerProvider>
-      <App />
-      <WindowsOutlet />
-    </WindowManagerProvider>
+    <MiniGBusProvider>
+      <BackButtonProvider>
+        <WindowManagerProvider>
+          <App />
+          <WindowsOutlet />
+        </WindowManagerProvider>
+      </BackButtonProvider>
+    </MiniGBusProvider>
   </StrictMode>
 );
