302687d74006cc6a5bb91b52b6f528595ef6dfd1
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-05-02 09:28:24 -0300

    feat: discrete disconnected/connecting/connected animations

--- src/Views/connection/components/scan_animation_graphic/index.tsx
+++ src/Views/connection/components/scan_animation_graphic/index.tsx
@@ -1,6 +1,22 @@
 import { cn } from "../../../../Lib/class_names";
+import doSwitch from "../../../../Lib/switch_expression";
 import style from "./style.module.css";
 
-export default function ScanAnimationGraphic(props: { className?: string }) {
-  return <div className={cn(style.visual2, props.className)} />;
+export default function ScanAnimationGraphic(props: {
+  className?: string;
+  connectionState: "NotConnected" | "Connecting" | "Connected";
+}) {
+  return (
+    <div
+      className={cn(
+        style.visual2,
+        doSwitch(props.connectionState, {
+          NotConnected: "",
+          Connecting: style.connecting,
+          Connected: style.connected,
+        }),
+        props.className
+      )}
+    />
+  );
 }
--- src/Views/connection/connection.tsx
+++ src/Views/connection/connection.tsx
@@ -1,7 +1,13 @@
 import { motion, Variant } from "framer-motion";
+import { useEffect } from "react";
 import VirtualBackButton from "../../Components/VirtualBackButton";
+import useBluetoothConnection from "../../Lib/bluetooth";
+import { useWindowing } from "../../Lib/compass_navigator";
+import useCurrentWindowKey from "../../Lib/compass_navigator/window_container/current_window_key_context";
 import useProvideCurrentWindow from "../../Lib/compass_navigator/window_container/use_provide_current_window";
+import Run from "../../Lib/run";
 import useKeepAwake from "../../Lib/use_keep_awake";
+import useUpdateEffect from "../../Lib/use_update_effect";
 import ScanAnimationGraphic from "./components/scan_animation_graphic";
 
 const variants = {
@@ -35,12 +41,27 @@ export default function Connection(props: ConnectionProps) {
     backButtonHandler: (exit) => exit(),
   });
 
+  const currentWindowKey = useCurrentWindowKey();
+  const windowing = useWindowing();
+
   useKeepAwake();
+
+  const ble = useBluetoothConnection();
+
+  useEffect(() => {
+    if (ble.state === "NotConnected") ble.setupConnection();
+  }, []);
+
+  useUpdateEffect(() => {
+    if (ble.state === "Connected") {
+      windowing.removeWindow(currentWindowKey);
+    }
+  }, [ble.state]);
 
   return (
     <div className="h-full w-full bg-white/20 backdrop-blur-sm">
       <motion.div
-        className="shadow-pixel-sm border-grey-800 fixed top-1/2 left-1/2 flex h-2/3 w-[calc(100%-theme('spacing.8'))] max-w-96 -translate-x-1/2 -translate-y-1/2 flex-col border bg-white p-4 font-serif"
+        className="shadow-pixel-sm border-grey-800 fixed top-1/2 left-1/2 flex h-2/3 w-[calc(100%-theme('spacing.8'))] -translate-x-1/2 -translate-y-1/2 flex-col border bg-white p-4 font-serif"
         initial="enter"
         animate="active"
         exit="exit"
@@ -50,8 +71,17 @@ export default function Connection(props: ConnectionProps) {
           <h1 className="text-2xl">Conectar ao Hardware</h1>
         </header>
         <main className="flex shrink grow flex-col items-center justify-center gap-8 overflow-y-scroll">
-          <ScanAnimationGraphic className="inline-block w-1/3" />
-          <h2>Procurando dispositivo...</h2>
+          <ScanAnimationGraphic className="inline-block w-1/3" connectionState={ble.state} />
+          {Run(() => {
+            switch (ble.state) {
+              case "NotConnected":
+                return <p>Desconectado</p>;
+              case "Connected":
+                return <p>A conexão está estabelecida.</p>;
+              case "Connecting":
+                return <p>Aguarde...</p>;
+            }
+          })}
         </main>
       </motion.div>
     </div>
